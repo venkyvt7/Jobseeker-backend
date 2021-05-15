ae6f0d985dd70c8c0f6db672a45b56af463c9ba3
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-05-16 01:58:05 +0530

    added offers

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express= require('express');
-const mongoose=require('mongoose');
-const cors=require('cors')
-
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const axios = require('axios')
+const app = express();
 const PORT = 3000;
-var offers=require('./data.js');
-var user= require('./control/routes/user');
-var admin= require('./control/routes/admin');
+var offers = require('./data.js');
+var user = require('./control/routes/user');
+var admin = require('./control/routes/admin');
 const { off } = require('./model/db/dbuserregister.js');
 
-mongoose.connect('mongodb://localhost/JobSeeker', {useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect('mongodb://localhost/JobSeeker', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(express.json());
@@ -17,10 +17,17 @@ app.use('/user', user);
 
 app.use('/admin', admin);
 
-app.get('/offers',(req,res)=>{
-  
- console.log(offers);
- res.send(offers);
+app.get('/offers', (req, res) => {
+
+
+   return axios.get('https://api.infojobs.net/api/7/offer', {
+        "Authorization": "Basic M2UxNjRiOWUwZmM2NGYwY2FlM2JlMjEzODBiYTBmMWE6RE10enREdldrd3hCOG02UVlremVCbEJLZFhWTFNnK0Iyd0hSUU40L25xTk9qZ1RLdEY="
+
+
+    }).then(res1=> res.send(res1.data)).catch();
+
+    console.log(offers);
+    res.send(offers);
     // res.send("hello")
 })
 
