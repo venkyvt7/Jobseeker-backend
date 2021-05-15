cb781f31d19fea94fbabc2441420b687aff1a371
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2021-05-15 22:27:29 +0530

    added data and res of offers

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const cors=require('cors')
 
 const app=express();
 const PORT = 3000;
-
+var offers=require('./data.js');
 var user= require('./control/routes/user');
 var admin= require('./control/routes/admin');
+const { off } = require('./model/db/dbuserregister.js');
 
 mongoose.connect('mongodb://localhost/JobSeeker', {useNewUrlParser: true, useUnifiedTopology: true } );
 
@@ -16,10 +17,11 @@ app.use('/user', user);
 
 app.use('/admin', admin);
 
-app.get('/',(req,res)=>{
+app.get('/offers',(req,res)=>{
   
-
-    res.send("hello")
+ console.log(offers);
+ res.send(offers);
+    // res.send("hello")
 })
 
 
---
+++ data.js
@@ -0,0 +1,181 @@
+const data=[
+    {
+        title: "Ingeniero de firmware",
+        link: "https://www.infojobs.net/massalfassar/ingeniero-firmware/of-i4819a98f704231ac018c09aa46730b",
+        city: "Massalfassar",
+
+
+
+    }
+    ,
+
+
+    {
+        title: "GESTOR DE PROYECTOS OBRA CIVIL",
+        link: "https://www.infojobs.net/pontevedra/gestor-proyectos-obra-civil/of-iaf6eb53b6f45ce8d8775dc6a227ae7",
+        city: "Pontevedra",
+     }
+    ,
+
+
+    {
+        title: "Marketing Redes Social y Gestor de Tienda Virtual",
+        link: "https://www.infojobs.net/fuenlabrada/marketing-redes-social-gestor-tienda-virtual/of-icca83b42604089a6b9f52eff1c0ebf",
+        city: "Fuenlabrada",
+
+
+
+    }
+    ,
+
+
+    {
+        title: "Front End Developer",
+        link: "https://www.infojobs.net/bilbao/front-end-developer/of-i6f1cff84ad4c9581540b6d158ea570",
+        city: "Bilbao",
+
+
+
+    },
+
+
+    {
+        title: "COMERCIAL APASIONADO, REPRESENTACIÓN ONGs.",
+        link: "https://www.infojobs.net/madrid/comercial-apasionado-representacion-ongs./of-i8df847df9f4132973f429a26b6ff3c",
+        city: "Madrid",
+
+
+
+    },
+
+
+    {
+        title: "Atención al cliente Multinacional Americana/ Imprescindible Noruego",
+        link: "https://www.infojobs.net/madrid/atencion-al-cliente-multinacional-americana-imprescindible-noruego/of-ie41563afe14b1dbf49f7e5fdd64eb5",
+        city: "Madrid",
+
+
+
+    },
+
+
+    {
+        title: "Ingeniero/a Eléctrico ePLAN",
+        link: "https://www.infojobs.net/bizkaia/ingeniero-electrico-eplan/of-i0f28e5bb124346ad6e32aa94e03f6f",
+        city: "Bizkaia",
+
+
+
+    },
+
+
+    {
+        title: "CAPTADOR/AS: Cto Indefinido Sueldo Fijo + Incentivos + Media Jornada",
+        link: "https://www.infojobs.net/alicante-alacant/captador-cto-indefinido-sueldo-fijo-incentivos-media-jornada/of-if873fe4b7e48f1a3d63bb500e6ed20",
+        city: "Logroño",
+
+
+
+    },
+
+
+    {
+        title: "PROMOTOR/A ONG OVIEDO: 1100€ (Fijo + Variable)  Media Jornada",
+        link: "https://www.infojobs.net/oviedo/promotor-ong-oviedo-1100-fijo-variable-media-jornada/of-i3dfb4f4f974092893b928ba733897d",
+        city: "Oviedo",
+
+
+
+    },
+
+
+    {
+        title: "POSITIVE MARKETING",
+        link: "https://www.infojobs.net/positive-marketing/em-i968157524526202550523214950852",
+        city: "Oviedo",
+
+
+
+    }
+    ,
+
+
+    {
+        title: "Ingeniero de firmware",
+        link: "https://www.infojobs.net/massalfassar/ingeniero-firmware/of-i4819a98f704231ac018c09aa46730b",
+        city: "Massalfassar",
+
+
+
+    },
+
+
+    {
+        title: "Versa Design",
+        link: "https://www.infojobs.net/versa-design/em-i98545257575248861011141009266297917179",
+        city: "Barcelona",
+
+
+
+    },
+
+
+    {
+        title: "Programador/a PLC Sénior",
+        link: "https://www.infojobs.net/sabadell/programador-plc-senior/of-ia55f02f06c48109bf01331e76e5a99",
+        city: "Sabadell",
+
+
+
+    }
+ 
+,  
+
+
+
+
+    {
+
+        title: "comercial venta materiales construcción",
+        link: "https://www.infojobs.net/badajoz/comercial-venta-materiales-construccion/of-icd221058404b7496277ea745fd410c",
+        city: "Badajoz",
+
+    }
+    ,  
+
+
+    {
+
+        title: "NATURE CERÁMICA",
+        link: "https://www.infojobs.net/nature-ceramica/em-i51515150575711568736594021033940805158",
+        city: "Badajoz",
+
+    }
+    ,  
+
+
+    {
+
+        title: "TUBERO-SOLDADOR",
+        link: "https://www.infojobs.net/valladolid/tubero-soldador/of-if90ab464744336a8c4e64f5cdef37c",
+        city: "Valladolid",
+
+    }
+    ,  
+
+
+    {
+
+        title: "Jefe/a Grupo Construcción experiencia hotelería",
+        link: "https://www.infojobs.net/valencia/jefe-grupo-construccion-experiencia-hoteleria/of-i8717ad289c4e0f8d5958860fab0f2d",
+        city: "Valencia",
+
+    }
+
+
+]
+
+
+
+
+module.exports=data;
